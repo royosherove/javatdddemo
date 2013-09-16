49f9f5c1dab71b8da127973b3c5fe56360df6ee6
Roy Osherove <roy@osherove.com>
2013-09-16 13:16:22 +0200

    first feature empty stirng

--- src/com/aout/kata/StringCalculator.java
+++ src/com/aout/kata/StringCalculator.java
@@ -1,4 +1,7 @@
 package com.aout.kata;
 
 public class StringCalculator {
+    public int add(String numbers) {
+        return 0;
+    }
 }
--- src/com/aout/kata/unittests/StringCalculatorTests.java
+++ src/com/aout/kata/unittests/StringCalculatorTests.java
@@ -1,5 +1,6 @@
 package com.aout.kata.unittests;
 
+import com.aout.kata.StringCalculator;
 import junit.framework.Assert;
 import org.junit.Test;
 
@@ -7,8 +8,13 @@ import static junit.framework.Assert.*;
 
 
 public class StringCalculatorTests {
+
     @Test
     public void add_emptyString_returnsZero(){
-        assertTrue(true);
+        StringCalculator sc = new StringCalculator();
+
+        int result = sc.add("");
+
+        assertEquals(0, result);
     }
 }
