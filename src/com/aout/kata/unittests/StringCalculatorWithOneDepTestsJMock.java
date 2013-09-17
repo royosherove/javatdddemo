25477ef192314b7ec4ffb845853f0e542483b3a7
Roy Osherove <roy@osherove.com>
2013-09-17 16:01:19 +0200

    progress

--- src/com/aout/kata/unittests/StringCalculatorWithOneDepTestsJMock.java
+++ src/com/aout/kata/unittests/StringCalculatorWithOneDepTestsJMock.java
@@ -2,23 +2,22 @@ package com.aout.kata.unittests;
 
 import com.aout.kata.Logger;
 import com.aout.kata.StringCalculatorWithOneDep;
+import org.jmock.Mockery;
 import org.junit.Test;
 
 import static org.easymock.EasyMock.createMock;
 import static org.easymock.EasyMock.replay;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 
-public class StringCalculatorWithOneDepTestsEasyMock {
+public class StringCalculatorWithOneDepTestsJMock {
 
 
     @Test
     public void add_whenCalled_callsLogger() throws Throwable {
-        Logger mockLog = createMock(Logger.class);
+        Mockery context = new Mockery();
+        Logger mockLog = context.mock(Logger.class);
         StringCalculatorWithOneDep sc = new StringCalculatorWithOneDep(mockLog);
 
-        //replay(mockLog);
         sc.add("1"); //BANG!
     }
 
